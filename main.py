9a89bbb5f2ccb9c71689fc7b71a85ae91778fde3
Vincent Charpentier <vincent.charpentier@student.uantwerpen.be>
2021-03-11 19:19:27 +0100

    Followed coding convention

--- main.py
+++ main.py
@@ -4,15 +4,7 @@
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 from Decoder import CamDecoder
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    #print_hi('PyCharm')
 
     decoder = CamDecoder(file_path='/Users/vincentcharpentier/Downloads/username.csv', output_file_path='/Users/vincentcharpentier/School/Master/MAP/Decoder/CAMv1.json')
     decoder.encode()
