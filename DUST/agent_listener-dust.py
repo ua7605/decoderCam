e1b74eda2ec98ef4d09d3f33a86b8d2e393ecc85
Vincent Charpentier <vincent.charpentier@student.uantwerpen.be>
2021-03-17 19:09:41 +0100

    Working on the incoming messages.

---
+++ DUST/agent_listener-dust.py
@@ -0,0 +1,12 @@
+from DUST.dust_Controller import DUSTController
+
+
+class AgentListenerDust(object):
+    def __init__(self, configuration_file_toml):
+        self.dust_comm: DUSTController = DUSTController.load_from_config(configuration=configuration_file_toml)
+
+    def _register_listener(self):
+        self.dust_comm.register_listener("cam-data-received-to-cam-server") # Todo Make in the DUST configuration.json a topic with name cam-data-received-to-cam-server
+
+    def incoming_DUST_message(self, data_hex):
+        print("Got message on topic")
