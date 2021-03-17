587c6a1bd80cecee1ec0ce67258112551e4b10a7
Vincent Charpentier <vincent.charpentier@student.uantwerpen.be>
2021-03-17 19:18:35 +0100

    Added the proper method call in the registry

--- DUST/agent_listener-dust.py
+++ DUST/agent_listener-dust.py
@@ -6,7 +6,7 @@ class AgentListenerDust(object):
         self.dust_comm: DUSTController = DUSTController.load_from_config(configuration=configuration_file_toml)
 
     def _register_listener(self):
-        self.dust_comm.register_listener("ivi_topic_out") # Todo Make in the DUST configuration.json a topic with name cam-data-received-to-cam-server
+        self.dust_comm.register_listener("ivi_topic_out", self.incoming_DUST_message) # Todo Make in the DUST configuration.json a topic with name cam-data-received-to-cam-server
 
     def incoming_DUST_message(self, data_hex):
         print("Got message on topic")
