315c101bfd208e05a4e707b44683758d09a9a59d
Vincent Charpentier <vincent.charpentier@student.uantwerpen.be>
2021-03-17 19:58:12 +0100

    Added support to write it out to json File.

--- DUST/dust_messages_decoder/dust_cam_decoder.py
+++ DUST/dust_messages_decoder/dust_cam_decoder.py
@@ -4,8 +4,9 @@ import json
 
 class DUSTCamDecoder(object):
 
-    def __init__(self):
+    def __init__(self, output_file):
         self.cam = asn1tools.compile_files('./cam121.asn', 'uper')
+        self.output_file = output_file
 
     def decode_cam_message(self, message):
         encoded_cam = bytearray.fromhex(message)
@@ -13,4 +14,6 @@ class DUSTCamDecoder(object):
         json_object_cam_message = json.dumps(decoded_cam)
         return json_object_cam_message
 
-
+    def _write_it_to_json_file(self, cam_message_json_format):
+        with open(self.output_file, 'w') as output_file:
+            output_file.write(cam_message_json_format)
