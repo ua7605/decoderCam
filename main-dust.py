97fd63a4722fe6e4a80bf574fede1ea46a95c259
Vincent Charpentier <vincent.charpentier@student.uantwerpen.be>
2021-03-19 17:40:50 +0100

    Trying to find the bug

--- main-dust.py
+++ main-dust.py
@@ -15,5 +15,5 @@ if __name__ == "__main__":
         print("The NAME IS: ", name)
         print("Toml file is successful being read")
     if is_true:
-        agent_dust = AgentListenerDust(configuration_toml=configuration_toml_file)
+        agent_dust = AgentListenerDust(configuration_toml=config_file)
         agent_dust.start()
