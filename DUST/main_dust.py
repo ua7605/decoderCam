5e4c382332543c2b31f2280766d978fd0a7a5058
Vincent Charpentier <vincent.charpentier@student.uantwerpen.be>
2021-03-19 17:10:20 +0100

    Problem with IndentationError

--- DUST/main_dust.py
+++ DUST/main_dust.py
@@ -5,14 +5,10 @@ import toml
 from DUST.agent_listener_dust import AgentListenerDust
 if __name__ == "__main__":
     configuration_toml_file = sys.argv[0]
-    is_true = False
     with open(configuration_toml_file) as file:
         config_file = toml.load(file)
-        is_true = True
-    if is_true:
-        agent_dust = AgentListenerDust(configuration_toml=config_file)
-        agent_dust.start()
-    else:
-        print("Error with the reading in the config file")
+
+    agent_dust = AgentListenerDust(configuration_toml=config_file)
+    agent_dust.start()
 
 
