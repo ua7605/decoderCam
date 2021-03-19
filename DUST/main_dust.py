569035c4fbc7a29f6d02a3b48eee5f8490ba76ed
Vincent Charpentier <vincent.charpentier@student.uantwerpen.be>
2021-03-19 17:07:10 +0100

    Hot fix: Added support to read in the toml file.

--- DUST/main_dust.py
+++ DUST/main_dust.py
@@ -1,8 +1,18 @@
 import sys
 
-from DUST.agent_listener_dust import AgentListenerDust
+import toml
 
+from DUST.agent_listener_dust import AgentListenerDust
 if __name__ == "__main__":
     configuration_toml_file = sys.argv[0]
-    agent_dust = AgentListenerDust(configuration_toml=configuration_toml_file)
-    agent_dust.start()
+    is_true = False
+    with open(configuration_toml_file) as file:
+        config_file = toml.load(file)
+        is_true = True
+    if is_true:
+        agent_dust = AgentListenerDust(configuration_toml=config_file)
+        agent_dust.start()
+    else:
+        print("Error with the reading in the config file")
+
+
--- main-dust.py
+++ main-dust.py
@@ -1,8 +1,16 @@
 import sys
 
+import toml
+
 from DUST.agent_listener_dust import AgentListenerDust
 
 if __name__ == "__main__":
     configuration_toml_file = sys.argv[0]
+
+    with open(configuration_toml_file) as file:
+        config_file = toml.load(f=file)
+        is_true = True
+    if is_true:
+
     agent_dust = AgentListenerDust(configuration_toml=configuration_toml_file)
     agent_dust.start()
