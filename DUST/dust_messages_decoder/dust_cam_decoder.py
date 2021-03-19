2db059dc0e21055dc20931b0f54de67b9533eef4
Vincent Charpentier <vincent.charpentier@student.uantwerpen.be>
2021-03-19 20:07:35 +0100

    Hot fix the channel and topic "CAM-topic-decoder" wasn't int het DUST configuration.json file.

--- DUST/dust_messages_decoder/dust_cam_decoder.py
+++ DUST/dust_messages_decoder/dust_cam_decoder.py
@@ -19,7 +19,6 @@ class DUSTCamDecoder(object):
         return json_object_cam_message
 
     def _write_it_to_json_file(self, cam_message_json_format):
-        print("The file were the result will be Written: ", self.json_file)
         with open(self.json_file, 'w') as output_json_file:
             output_json_file.write(cam_message_json_format)
             print("It is written to the file!!!! ")
