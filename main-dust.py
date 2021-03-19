2734048eedd54c574caa7e4eca376b38b187920d
Vincent Charpentier <vincent.charpentier@student.uantwerpen.be>
2021-03-19 17:17:10 +0100

    Possible bug resolved

--- main-dust.py
+++ main-dust.py
@@ -6,7 +6,7 @@ from DUST.agent_listener_dust import AgentListenerDust
 
 if __name__ == "__main__":
     configuration_toml_file = sys.argv[0]
-
+    is_true = False
     with open(configuration_toml_file) as file:
         config_file = toml.load(f=file)
         is_true = True
