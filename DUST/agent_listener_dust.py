1a00927d0b821ef43f9f3826f0e2995dfec7e7b7
Vincent Charpentier <vincent.charpentier@student.uantwerpen.be>
2021-03-22 11:01:45 +0100

    Made a comment to make it clear it already works

--- DUST/agent_listener_dust.py
+++ DUST/agent_listener_dust.py
@@ -3,7 +3,7 @@ import time
 from DUST.dust_Controller import DUSTController
 from DUST.dust_messages_decoder.dust_cam_decoder import DUSTCamDecoder
 
-
+# Tested on Testbed and it works!
 # With this everything DUST related will started so just make a object of this class and interact with the agent.
 class AgentListenerDust(object):
     def __init__(self, configuration_toml):
