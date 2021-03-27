e4e9ba4a5ad1166a26613995c9057bffe3284179
Vincent Charpentier <vincent.charpentier@student.uantwerpen.be>
2021-03-27 19:22:30 +0100

    Solving boost error

--- main-hybrid-service.py
+++ main-hybrid-service.py
@@ -2,7 +2,7 @@ import sys
 import time
 
 import toml
-import logging
+#import logging
 
 from DUST.agent_listener_dust import AgentListenerDust
 from DUST.CAM_Message_generator.cam_generator import CamGenerator
@@ -11,7 +11,7 @@ from tools.startup_phase import Keyword
 
 if __name__ == "__main__":
 
-    logging.basicConfig(format='%(asctime)s %(message)s')
+    #logging.basicConfig(format='%(asctime)s %(message)s')
     configuration_toml_file = "config.toml"
     current_phase = Startup.CONFIG_FILE_CHECK.value
 
@@ -21,9 +21,8 @@ if __name__ == "__main__":
             with open(configuration_toml_file) as file:
                 config_file = toml.load(f=file)
                 current_phase = Startup.CONFIG_FILE_READING.value
-                logging.warning("Configuration file is successfully being read")
+                #logging.warning("Configuration file is successfully being read")
         except:
-            logging.error("File doesn't exists: "+configuration_toml_file)
             #print("File doesn't exists: "+configuration_toml_file)
             sys.exit()
 
@@ -38,7 +37,7 @@ if __name__ == "__main__":
         is_service_sp_vehicle: str = usage_config["special_vehicle_service"]
 
         if decoder.__eq__(Keyword.true.name) and cam_generator.__eq__(Keyword.false.name):
-            logging.warning("The decoder will be starting up")
+            #logging.warning("The decoder will be starting up")
 
             agent_dust = AgentListenerDust(configuration_toml=config_file)
             agent_dust.start()
@@ -46,26 +45,26 @@ if __name__ == "__main__":
                 time.sleep(1)
 
         elif decoder.__eq__(Keyword.false.name) and cam_generator.__eq__(Keyword.true.name):
-            logging.warning("The message generator service will be started")
+            #logging.warning("The message generator service will be started")
             agent_dust = AgentListenerDust(configuration_toml=config_file)
             message_generator = CamGenerator(AgentListener=agent_dust)
-            # if is_service_sp_vehicle.__eq__(Keyword.true.name):
-            #     message_generator.start_v2_custom_massaging()
-            # else:
-            #     message_generator.start_custom_massaging()
+            if is_service_sp_vehicle.__eq__(Keyword.true.name):
+                message_generator.start_v2_custom_massaging()
+            else:
+                message_generator.start_custom_massaging()
             message_generator.start_custom_massaging()
         elif decoder.__eq__(Keyword.true.name) and cam_generator.__eq__(Keyword.true.name):
-            logging.warning("The message generator service and CAM decoding service will be started")
+            #logging.warning("The message generator service and CAM decoding service will be started")
             agent_dust = AgentListenerDust(configuration_toml=config_file)
             agent_dust.start()
             message_generator = CamGenerator(AgentListener=agent_dust)
-            # if is_service_sp_vehicle.__eq__(Keyword.true.name):
-            #     message_generator.start_v2_custom_massaging()
-            # else:
-            #     message_generator.start_custom_massaging()
+            if is_service_sp_vehicle.__eq__(Keyword.true.name):
+                message_generator.start_v2_custom_massaging()
+            else:
+                message_generator.start_custom_massaging()
             message_generator.start_custom_massaging()
         else:
-            logging.warning("Application stopped")
+            #logging.warning("Application stopped")
             sys.exit()
     #
     # with open(configuration_toml_file) as file:
