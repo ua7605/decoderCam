f5d037efa370ffa2f9a8cbd9b4ad0363f8eaa89b
Vincent Charpentier <vincent.charpentier@student.uantwerpen.be>
2021-03-27 19:56:52 +0100

    Solving boost error: when you use the custom cam header emergency vehicle

--- DUST/dust_messages_decoder/dust_cam_decoder.py
+++ DUST/dust_messages_decoder/dust_cam_decoder.py
@@ -12,10 +12,15 @@ class DUSTCamDecoder(object):
     def decode_cam_message(self, message):
         # TODO Make it possible to distinguish CAM, DENM and IVI messages
         encoded_cam = bytearray(message)
+        print("A: Do you come still here?")
         print(encoded_cam)
+        print("B: Do you come still here?")
         decoded_cam = self.cam.decode('CAM', encoded_cam)
+        print("C: Do you come still here?")
         json_object_cam_message = json.dumps(decoded_cam)
+        print("D: Do you come still here?")
         self._write_it_to_json_file(cam_message_json_format=json_object_cam_message)
+        print("E: Do you come still here?")
         return json_object_cam_message
 
     def _write_it_to_json_file(self, cam_message_json_format):
