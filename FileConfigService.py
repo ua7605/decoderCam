c427921e441815720be3727d1e7f18ae22809b63
Vincent Charpentier <vincent.charpentier@student.uantwerpen.be>
2021-04-25 15:34:35 +0200

    Style: followed Python style convention to be conform with the standard in FileConfigService.py.

--- FileConfigService.py
+++ FileConfigService.py
@@ -1,14 +1,13 @@
 class FileConfig:
 
     def __new__(cls, *args, **kwargs):
-        if not hasattr(cls,'instance'):
+        if not hasattr(cls, 'instance'):
             cls.instance = super(FileConfig, cls).__new__(cls)
         return cls.instance
 
     def __init__(self, file_path_to_csv_file, output_file_json):
         self.file_path_to_csv_file = file_path_to_csv_file
         self.output_file_json = output_file_json
-
 
     def get_file_path_to_csv_file(self):
         return self.file_path_to_csv_file
