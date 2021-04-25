eade385c44a5026bda1bae3f6593f969f2fe92db
Vincent Charpentier <vincent.charpentier@student.uantwerpen.be>
2021-04-25 15:30:35 +0200

    Style: make an update to style of the main-hybrid-service.py file. To be comform with the Python code convention.

--- main-hybrid-service.py
+++ main-hybrid-service.py
@@ -1,7 +1,7 @@
 import sys
 import time
 import toml
-#import logging
+# import logging
 
 from DUST.agent_listener_dust import AgentListenerDust
 from DUST.CAM_Message_generator.cam_generator import CamGenerator
@@ -10,7 +10,7 @@ from tools.startup_phase import Keyword
 
 if __name__ == "__main__":
 
-    #logging.basicConfig(format='%(asctime)s %(message)s')
+    # logging.basicConfig(format='%(asctime)s %(message)s')
     configuration_toml_file = "config.toml"
     current_phase = Startup.CONFIG_FILE_CHECK.value
 
@@ -20,9 +20,9 @@ if __name__ == "__main__":
             with open(configuration_toml_file) as file:
                 config_file = toml.load(f=file)
                 current_phase = Startup.CONFIG_FILE_READING.value
-                #logging.warning("Configuration file is successfully being read")
+                # logging.warning("Configuration file is successfully being read")
         except:
-            #print("File doesn't exists: "+configuration_toml_file)
+            # print("File doesn't exists: "+configuration_toml_file)
             sys.exit()
 
     if Startup.CONFIG_FILE_READING.value.__eq__(current_phase):
@@ -38,7 +38,7 @@ if __name__ == "__main__":
         false = Keyword.false.name
 
         if decoder.__eq__(true) and cam_generator.__eq__(false) and is_service_sp_vehicle.__eq__(false):
-            #logging.warning("The decoder will be starting up")
+            # logging.warning("The decoder will be starting up")
 
             agent_dust = AgentListenerDust(configuration_toml=config_file)
             agent_dust.start()
@@ -46,7 +46,7 @@ if __name__ == "__main__":
                 time.sleep(1)
 
         elif decoder.__eq__(false) and cam_generator.__eq__(true):
-            #logging.warning("The message generator service will be started")
+            # logging.warning("The message generator service will be started")
             agent_dust = AgentListenerDust(configuration_toml=config_file)
             message_generator = CamGenerator(AgentListener=agent_dust)
             if is_service_sp_vehicle.__eq__(Keyword.true.name):
@@ -64,7 +64,7 @@ if __name__ == "__main__":
                 time.sleep(1)
 
         elif decoder.__eq__(true) and cam_generator.__eq__(true):
-            #logging.warning("The message generator service and CAM decoding service will be started")
+            # logging.warning("The message generator service and CAM decoding service will be started")
             agent_dust = AgentListenerDust(configuration_toml=config_file)
             agent_dust.start()
             message_generator = CamGenerator(AgentListener=agent_dust)
@@ -74,29 +74,5 @@ if __name__ == "__main__":
                 message_generator.start_custom_massaging()
             message_generator.start_custom_massaging()
         else:
-            #logging.warning("Application stopped")
+            # logging.warning("Application stopped")
             sys.exit()
-    #
-    # with open(configuration_toml_file) as file:
-    #     config_file = toml.load(f=file)
-    #     is_true = True
-    #     dust_config = config_file["DUST"]
-    #     name = dust_config["application_name"]
-    #     print("The NAME IS: ", name)
-    #     print("Toml file is successful being read")
-    #
-    #     usage_config = config_file["usage"]
-    #     decoder = usage_config["cam_decoder"]
-    #     cam_generator = usage_config["cam_generator"]
-    #
-    # if decoder == "true":
-    #     agent_dust = AgentListenerDust(configuration_toml=config_file)
-    #     agent_dust.start()
-    # elif cam_generator == "true":
-    #     agent_dust = AgentListenerDust(configuration_toml=config_file)
-    #     message_generator = CamGenerator(AgentListener=agent_dust)
-    #     message_generator.start_custom_massaging()
-    # print("Agent is started correct! and is listening and can sent")
-    # while True:
-    #     time.sleep(1)
-    #
