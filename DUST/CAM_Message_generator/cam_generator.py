614767898bb76fabf24aa53ec9254356db5fa910
Vincent Charpentier <vincent.charpentier@student.uantwerpen.be>
2021-05-17 15:56:32 +0200

    style: added in the CamGenerator class python doc.

--- DUST/CAM_Message_generator/cam_generator.py
+++ DUST/CAM_Message_generator/cam_generator.py
@@ -7,11 +7,19 @@ from DUST.agent_listener_dust import AgentListenerDust
 from tools.startup_phase import LightBar
 
 
-# TODO: Add in the future from the GPS location the longitude and latitude with the messages.
 class CamGenerator(object):
+    """
+    When the decoder is being used as a CAM message generator an object of this class needs to be made.
+    The content of the CAM messages that will be sent can be specified in the "cam_template.json" file.
+    """
     dir_name = os.path.dirname(__file__)
 
     def __init__(self, AgentListener, dir_name=dir_name):
+        """
+
+        :param AgentListener: the DUST agent.
+        :param dir_name: current directory were this python file is located.
+        """
         self.cam_asn1 = asn1tools.compile_files('./cam121.asn', 'uper')
         template_cam_json = os.path.join(dir_name, "cam_template.json")  # "cam_template.json")
         f = open(template_cam_json, "r")
@@ -19,6 +27,9 @@ class CamGenerator(object):
         self.agent_listener_dust: AgentListenerDust = AgentListener
 
     def start_custom_massaging(self):
+        """
+        To start the sending CUSTOM CAM message from the template.
+        """
         # custom_message = self.template_cam
         i = 1
         while True:
@@ -35,6 +46,11 @@ class CamGenerator(object):
             print(out)
 
     def start_special_vehicle_service_massaging(self, dir_name=dir_name):
+        """
+        When CAM messages needs to be generated for special vehicles use the "cam_custom_template.json" file to set the
+        content you want that being sent.
+        :param dir_name: were the python file is located.
+        """
         i = 1
         template_cam_json = os.path.join(dir_name, "cam_custom_template.json")  # "cam_template.json")
         f = open(template_cam_json, "r")
@@ -57,6 +73,10 @@ class CamGenerator(object):
             print(custom)
 
     def _service_setup(self):
+        """
+        Makes it possible to make a json file otherwise errors would be generated.
+        :return: the correct message to make a json object without errors.
+        """
         self.template_cam['cam']['camParameters']['highFrequencyContainer'] = \
             (
                 'basicVehicleContainerHighFrequency',
@@ -72,6 +92,10 @@ class CamGenerator(object):
             message=self.template_cam['cam']['camParameters']['specialVehicleContainer'][1]['lightBarSirenInUse'])
 
     def _light_bar_siren_in_use(self, message):
+        """
+        To add the value of the siren or light if they are active.
+        :param message: CAM message
+        """
         print("message: ", message)
 
         light_bar_active = message['lightBarActivated']
@@ -91,6 +115,9 @@ class CamGenerator(object):
                 'lightBarSirenInUse'] = LightBar.NONE.value
 
     def start_special_vehicle_service_massaging_old(self, dir_name=dir_name):
+        """
+        DEPRECATED you can ignore this.
+        """
         i = 1
         template_cam_json = os.path.join(dir_name, "cam_custom_template.json")  # "cam_template.json")
         f = open(template_cam_json, "r")
