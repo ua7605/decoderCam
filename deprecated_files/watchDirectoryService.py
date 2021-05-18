b460a60d0a8bd125e1c263059084fde523c7ffde
Vincent Charpentier <vincent.charpentier@student.uantwerpen.be>
2021-05-18 15:43:25 +0200

    style: code clean up in watchDirectoryService.py file.

--- deprecated_files/watchDirectoryService.py
+++ deprecated_files/watchDirectoryService.py
@@ -1,29 +1,9 @@
-import sys
-import time
 import logging
-from watchdog.observers import Observer
-from watchdog.events import LoggingEventHandler
+import os.path
+import time
 
-# if __name__ == "__main__":
-#     logging.basicConfig(level=logging.INFO,
-#                         format='%(asctime)s - %(message)s',
-#                         datefmt='%Y-%m-%d %H:%M:%S')
-#     #path = sys.argv[1] if len(sys.argv) > 1 else '.'
-#     path = '/Users/vincentcharpentier/School/Master/MAP/DirToWatch'
-#     event_handler = LoggingEventHandler()
-#     observer = Observer()
-#     observer.schedule(event_handler, path, recursive=True)
-#     observer.start()
-#     try:
-#         while True:
-#             time.sleep(1)
-#     finally:
-#         observer.stop()
-#         observer.join()
-
-import sys, os.path, time, logging
-from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
+from watchdog.observers import Observer
 
 
 class MyEventHandler(PatternMatchingEventHandler):
@@ -43,15 +23,15 @@ class MyEventHandler(PatternMatchingEventHandler):
         super(MyEventHandler, self).on_modified(event)
         logging.info("File %s was just modified" % event.src_path)
 
-def main(file_path=None):
 
+def main(file_path=None):
     logging.basicConfig(level=logging.INFO,
-        format='%(asctime)s - %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S')
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
     watched_dir = os.path.split(file_path)[0]
-    print ('watched_dir = {watched_dir}'.format(watched_dir=watched_dir))
+    print('watched_dir = {watched_dir}'.format(watched_dir=watched_dir))
     patterns = [file_path]
-    print ('patterns = {patterns}'.format(patterns=', '.join(patterns)))
+    print('patterns = {patterns}'.format(patterns=', '.join(patterns)))
     event_handler = MyEventHandler(patterns=patterns)
     observer = Observer()
     observer.schedule(event_handler, watched_dir, recursive=True)
@@ -64,7 +44,5 @@ def main(file_path=None):
     observer.join()
 
 
-
 if __name__ == "__main__":
-
     main(file_path='/Users/vincentcharpentier/School/Master/MAP/DirToWatch')
