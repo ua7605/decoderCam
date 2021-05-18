ce56cf111aee3ef4aaf48f2dccf61ee793dc5cf4
Vincent Charpentier <vincent.charpentier@student.uantwerpen.be>
2021-05-18 15:47:30 +0200

    style: code clean up in main.py file.

--- deprecated_files/main.py
+++ deprecated_files/main.py
@@ -1,39 +1,31 @@
-# This is a sample Python script.
+import getopt
+import sys
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 import watchFileService
-import sys, getopt
-# if __name__ == '__main__':
-#
-#     decoder = CamDecoder(file_path='/Users/vincentcharpentier/School/Master/MAP/Decoder/cam_1_20210312T104241_uper.csv', output_file_path='/Users/vincentcharpentier/School/Master/MAP/Decoder/CAMv1.json')
-#     #decoder.encode()
+
 
 def main(argv):
-   inputfile = ''
-   outputfile = ''
-   try:
-      opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
-   except getopt.GetoptError:
-      print ('test.py -i <inputfile> -o <outputfile>')
-      sys.exit(2)
-   for opt, arg in opts:
-      if opt == '-h':
-         print('test.py -i <inputfile> -o <outputfile>')
-         sys.exit()
-      elif opt in ("-i", "--ifile"):
-         inputfile = arg
-      elif opt in ("-o", "--ofile"):
-         outputfile = arg
-   print('Input file is "', inputfile)
-   print('Output file is "', outputfile)
-   watchFileService.main(file_path=inputfile, file_path_json=outputfile)
+    # You need to give as arguments first an input file and then an output file
 
+    input_file = ''
+    output_file = ''
+    try:
+        opts, args = getopt.getopt(argv, "hi:o:", ["ifile=", "ofile="])
+    except getopt.GetoptError:
+        print('test.py -i <input_file> -o <output_file>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('test.py -i <input_file> -o <output_file>')
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            input_file = arg
+        elif opt in ("-o", "--ofile"):
+            output_file = arg
+    print('Input file is "', input_file)
+    print('Output file is "', output_file)
+    watchFileService.main(file_path=input_file, file_path_json=output_file)
 
 
 if __name__ == "__main__":
     main(sys.argv[1:])
-    #watchFileService.main(file_path='/Users/vincentcharpentier/School/Master/MAP/Decoder/cam_1_20210312T104241_uper.csv', file_path_json='/Users/vincentcharpentier/School/Master/MAP/Decoder/CAMv1.json')
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/   /Users/vincentcharpentier/Downloads/username.csv
